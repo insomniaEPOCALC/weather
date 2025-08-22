ce1fa5ca8bc8c4e3faf5eea04413a76006b30e76
insomniaEPOCALC <pi-opal@outlook.jp>
2025-08-22 18:17:07 +0900

    add fonts & styles

--- resources/views/forecasts/top.blade.php
+++ resources/views/forecasts/top.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
 
-    <h1>天気予報</h1>
-    <h2>地点を選択</h2>
-    <div class='points'>
+    <h1 class="text-6xl text-center">天気予報</h1>
+    <h2 class="text-center">地点を選択</h2>
+    <div class='text-center'>
 
         @foreach($places as $place)
             <a href="weather/{{ $place->api_id }}">{{ $place->name }}</a>
@@ -14,7 +14,7 @@
     </div>
 
 
-    <h2>郵便番号で都道府県を検索</h2>
+    <h2 class='text-center'>郵便番号で都道府県を検索</h2>
     <form action="/search" method="GET" class="search-form">
         <input type="text" name="query" class="search-input" value="{{request('query')}}">
         <button type="submit" class="search-button">検索</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @yield('style')
+    @yield('script')
+
     <head>
-        <meta charset="utf-8">
-        <title>@yield('title')</title>
-        @yield('style')
-        @yield('script')
-    <head>
-    <body>
-        <div>
+
+    <body class=' flex items-center justify-center'>
+        <div class='block'>
             @yield('content')
         </div>
     </body>
+
 </html>
