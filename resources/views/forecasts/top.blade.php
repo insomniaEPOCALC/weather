c2193c8eec801f3ba6980bedd6f0f95cdaa44494
insomniaEPOCALC <pi-opal@outlook.jp>
2025-08-25 12:01:49 +0900

    feat:rivision styles

--- resources/views/forecasts/top.blade.php
+++ resources/views/forecasts/top.blade.php
@@ -5,22 +5,26 @@
 @section('content')
 
     <h1 class="text-6xl text-center">天気予報</h1>
-    <h2 class="text-center">地点を選択</h2>
-    <div class='text-center'>
 
-        @foreach($places as $place)
-            <x-button :place="$place" />
-        @endforeach
+    <div class='block-inner'>
+        <h2 class="text-center">地点を選択</h2>
+        <div class='text-center'>
+
+            @foreach($places as $place)
+                <x-button :place="$place" />
+            @endforeach
+        </div>
     </div>
 
 
-    <h2 class='text-center'>郵便番号で都道府県を検索</h2>
-    <form action="/search" method="GET" class="search-form">
-        <input type="text" name="query" class="search-input" value="{{request('query')}}">
-        <button type="submit" class="search-button">検索</button>
-        @error('query')
-            <div style="color: red;">{{ $message }}</div>
-        @enderror
-    </form>
-
+    <div class='block-inner'>
+        <h2 class='text-center'>郵便番号で都道府県を検索</h2>
+        <form action="/search" method="GET" class="search-form flex justify-center">
+            <input type="text" name="query" class="search-input" value="{{request('query')}}">
+            <button type="submit" class="search-button">検索</button>
+            @error('query')
+                <div style="color: red;">{{ $message }}</div>
+            @enderror
+        </form>
+    </div>
 @endsection
